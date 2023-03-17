b780d61079cd444f0cee17a348edaa5a9eead288
GigaN1ga <loppupossum@gmail.com>
2023-03-17 13:49:52 +0600

    hw4

--- homeWorks/hw3.py
+++ homeWorks/hw3.py
@@ -1,34 +1,76 @@
 Abil = 100
-
-
-class Bank():
-    def __init__(self, name, balanse):
-        self.name = name
-        self.balanse = balanse
+class Bank:
+    def __init__(self, name, balance):
+        self._name = name
+        self._balance = balance
 
     def moneyX(self):
         add = int(input('Введите сумму которую хотите добавить на свой счёт: '))
-        self.balanse += add
-        print(self.balanse)
+        self._balance += add
+        print(self._balance)
 
     def kill(self):
-        return self.balanse == 0
+        return self._balance == 0
 
-    def __jackpot(self):
-        a = self.balanse * 10
+    def _jackpot(self):
+        a = self._balance * 10
         print(a)
 
     def __str__(self):
-        return f'Имя: {self.name}\n' \
-               f'Баланс: {self.balanse}'
+        return f'Имя: {self._name}\n' \
+               f'Баланс: {self._balance}'
 
-    def __unite_balanse(self):
-        print(self.balanse())
+    def _unite_balance(self):
+        print(self._balance)
         pass
 
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_balance(self):
+        return self._balance
+
+    def set_balance(self, balance):
+        self._balance = balance
 
 
-user = Bank('Abil', 0)
+class NewBank(Bank):
+    def __init__(self, name, balance):
+        super().__init__(name, balance)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, balance):
+        self._balance = balance
+
+
+user = NewBank('Abil', 0)
 print(user)
+
+user.set_name('Бека')
+print(user.get_name())
+
+user.name = 'Жаннат'
+print(user.name)
+
+user.set_balance(100)
+print(user.get_balance())
+
+user.balance = 200
+print(user.balance)
 
 
